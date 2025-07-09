ba7353188d04dfe6b110a1e5b4f33633e7171df8
tabor <815141681@qq.com>
2025-07-09 14:52:27 +0800

    PDF压缩实现拖拽功能

--- OfficeAssistant/Views/PDF/PdfCompressView.axaml.cs
+++ OfficeAssistant/Views/PDF/PdfCompressView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Input;
 using OfficeAssistant.ViewModels.PDF;
 
 namespace OfficeAssistant.Views.PDF
@@ -13,6 +14,29 @@ namespace OfficeAssistant.Views.PDF
             InitializeComponent();
             _viewModel = new PdfCompressViewModel();
             DataContext = _viewModel;
+            // 通过代码注册 Drop 和 DragOver 事件，确保控件已加载
+            this.AttachedToVisualTree += (s, e) =>
+            {
+                var listBox = this.FindControl<ListBox>("FileListBox");
+                if (listBox != null)
+                {
+                    listBox.AddHandler(DragDrop.DropEvent, ListBox_OnDrop, Avalonia.Interactivity.RoutingStrategies.Bubble);
+                    listBox.AddHandler(DragDrop.DragOverEvent, ListBox_OnDragOver, Avalonia.Interactivity.RoutingStrategies.Bubble);
+                }
+            };
+        }
+
+        // 处理拖拽悬停，显示可放置样式
+        private void ListBox_OnDragOver(object? sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.Files))
+            {
+                e.DragEffects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.DragEffects = DragDropEffects.None;
+            }
         }
 
         private async void SelectFiles(object sender, RoutedEventArgs e)
@@ -42,5 +66,26 @@ namespace OfficeAssistant.Views.PDF
         {
             _viewModel.ClearAllFiles();
         }
+        
+        // 支持拖拽文件到ListBox
+        private void ListBox_OnDrop(object? sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.Files))
+            {
+                var files = e.Data.GetFiles();
+                if (files != null)
+                {
+                    foreach (var file in files)
+                    {
+                        var path = file.Path.LocalPath;
+                        if (path.EndsWith(".pdf", System.StringComparison.OrdinalIgnoreCase))
+                        {
+                            if (!_viewModel.SelectedFiles.Contains(path))
+                                _viewModel.SelectedFiles.Add(path);
+                        }
+                    }
+                }
+            }
+        }        
     }
 }
